528ed5725cd185e6a1e81d7ba20cea54f476b4d2
Harshit Srivastava <32229132+harshit242016@users.noreply.github.com>
2022-02-11 01:21:49 +0530

    Time: 20 ms (67.48%), Space: 11.8 MB (23.11%) - LeetHub

---
+++ 265-paint-house-ii/265-paint-house-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minCostHelper(vector<vector<int>> &costs, vector<vector<int>> &dp,int index,int curr)
+    {
+        if(index == -1)
+        {
+            return 0;
+        }
+        if(dp[index][curr] != -1)
+        {
+            return dp[index][curr];
+        }
+        
+        int min_price = INT_MAX;
+        
+        for(int i = 0;i<costs[0].size();i++)
+        {
+            if(i!=curr)
+            {
+                int chose = costs[index][i] + minCostHelper(costs,dp,index-1,i);
+                min_price = min(chose,min_price);
+            }
+        }
+        
+        dp[index][curr] = min_price;
+        return dp[index][curr];
+        
+    }
+    
+    int minCostII(vector<vector<int>>& costs) 
+    {
+        int n = costs.size();
+        int m = costs[0].size();
+        vector<vector<int>> dp(n,vector<int>(m+1,-1));
+        return minCostHelper(costs,dp,n-1,m);
+    }
+};
