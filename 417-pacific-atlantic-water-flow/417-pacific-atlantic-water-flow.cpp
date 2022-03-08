7868d9b0a810ba3879411e7f3b936cf96bd7e626
Harshit Srivastava <32229132+harshit242016@users.noreply.github.com>
2022-03-09 01:27:11 +0530

    Time: 54 ms (67.40%), Space: 18.6 MB (58.52%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,54 +1,53 @@
 class Solution {
 public:
-    int m, n;
-
-    vector<vector<bool> > atlantic, pacific;
-	vector<vector<int> > ans;    
+    vector<vector<bool>> pacific,atlantic;
+    int n,m;
     
-    int dx[5] = {0,1,0,-1,0};
+    vector<vector<int>> ans;
+    int dir[5] = {0,1,0,-1,0};
     
-    vector<vector<int> > pacificAtlantic(vector<vector<int>>& heights) 
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) 
     {
-        m = heights.size(), n = heights[0].size();
-        atlantic = pacific = vector<vector<bool> >(m, vector<bool>(n, false));
-		// perform dfs from all edge cells (ocean-connected cells)
-        for(int i = 0; i < m; i++) 
+        n = heights.size();
+        m = heights[0].size();
+        
+        pacific = atlantic = vector<vector<bool>> (n,vector<bool>(m,false));
+         
+        for(int i = 0;i<n;i++)
         {
-            dfs(heights, pacific, i, 0); 
-            dfs(heights, atlantic, i, n - 1);
+            dfs(pacific,heights,i,0);
+            dfs(atlantic,heights,i,m-1);
         }
         
-        for(int i = 0; i < n; i++) 
+        for(int i = 0;i<m;i++)
         {
-            dfs(heights, pacific, 0, i);
-            dfs(heights, atlantic, m - 1, i);
+            dfs(pacific,heights,0,i);
+            dfs(atlantic,heights,n-1,i);
         }
         
         return ans;
     }
     
-    void dfs(vector<vector<int>> &heights,vector<vector<bool>> &visited,int x,int y)
+    void dfs(vector<vector<bool>> &visited,vector<vector<int>> &heights, int x, int y)
     {
-        if(visited[x][y] == true)
+        if(visited[x][y])
         {
             return;
         }
         visited[x][y] = true;
-        if(atlantic[x][y] && pacific[x][y])
+       //\U0001f9d0 
+        if(pacific[x][y] && atlantic[x][y])
         {
             ans.push_back(vector<int>{x,y});
         }
-        
-        int m = heights.size(), n = heights[0].size();
-        
-        for(int i=0; i<4; i++)
+        for(int i = 0;i<4;i++)
         {
-            int nx = x+dx[i], ny = y+dx[i+1];
-            if(nx<0 or nx>=m or ny<0 or ny>=n or heights[nx][ny] < heights[x][y]) 
+            int nx = x+dir[i] , ny = y+ dir[i+1];
+            if(nx<0 or nx>=n or ny<0 or ny>=m or heights[x][y]>heights[nx][ny])
             {
                 continue;
             }
-            dfs(heights,visited,nx, ny);
+            dfs(visited,heights,nx,ny);
         }
     }
     
