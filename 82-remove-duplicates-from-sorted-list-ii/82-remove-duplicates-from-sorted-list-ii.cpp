660abff274d2634ef5fe02318438d89b93884b8c
Harshit Srivastava <32229132+harshit242016@users.noreply.github.com>
2022-03-10 01:42:51 +0530

    Time: 16 ms (26.51%), Space: 10.9 MB (99.55%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -16,26 +16,29 @@ public:
         {
             return head;
         }
+        
         ListNode dummy = ListNode(0);
         dummy.next = head;
         ListNode *curr = head;
         ListNode *prev = &dummy;
+        
         while(curr!=NULL)
         {
-            while(curr->next != NULL and curr->val == curr->next->val)
+            while(curr->next!=NULL and curr -> val == curr->next->val)
             {
                 curr = curr->next;
             }
-            if(prev->next == curr)
+            if(prev -> next == curr)
             {
                 prev = prev->next;
             }
             else
             {
-                prev->next = curr->next;
+                prev -> next = curr->next;
             }
             curr = curr->next;
         }
+        
         return dummy.next;
     }
 };
