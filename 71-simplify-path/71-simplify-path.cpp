8de8160196edd430f18a606a6b83bfd776bcdb27
Harshit Srivastava <32229132+harshit242016@users.noreply.github.com>
2022-03-15 02:01:39 +0530

    Time: 4 ms (88.67%), Space: 8.4 MB (74.50%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string temp = "";
+            path = path + "/";
+            stack<string> s;
+            int n = path.length();
+            for (int i = 0; i < n; i++)
+            {
+                if (path[i] == '/')
+                {
+                    if (temp == "..")
+                    {
+                        if (s.size())
+                        {
+                            s.pop();
+                        }
+                    }
+                    else if (temp != "" && temp != ".")
+                    {
+                        s.push(temp);
+                    }
+                    temp = "";
+                }
+                else
+                {
+                    temp += path[i];
+                }
+            }
+            if (temp != "" && temp != ".")
+            {
+                s.push(temp);
+            }
+            temp = "";
+            temp += "/";
+            vector<string> ans;
+            while (!s.empty())
+            {
+                ans.push_back(s.top());
+                s.pop();
+            }
+            reverse(ans.begin(), ans.end());
+            for (int i = 0; i < ans.size(); i++)
+            {
+                temp += ans[i];
+                if (i != ans.size() - 1)
+                {
+                    temp += "/";
+                }
+            }
+
+            return temp;
+    }
+};
